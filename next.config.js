0eda5e5400bdd2c40b7c226f14766b58a87ab73c
@kevinjosephtpr <kevin.joseph.tpr@pwc.com>
2023-06-24 07:17:25 +0000

    altered key

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   env:{
     openAiApiKey : "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",
     organizationKey : "org-N9eWqy49Tup0hPDf3fmBpx34",
-    openAiApiKey:"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"
+    // openAiApiKey:"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"
   }
 }
 
